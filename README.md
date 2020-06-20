b1c9e3786e2f8e10962729d047201ab63ec72d8e
AlanSalas <dubekone@gmail.com>
2020-06-19 20:04:24 -0500

    update conexión de bd remota

--- README.md
+++ README.md
@@ -3,7 +3,3 @@
 Proyecto final 9no cuatrimestre
 
 composer install para instalar dependencias
-
-dentro de libs esta mi erp.sql pueden usarlo de base e ir actualizandolo con sus tablas
-
-la base de datos en local debe ser llamada "erp"
--- config/config.php
+++ config/config.php
@@ -4,10 +4,10 @@
 if ($_SERVER["HTTP_HOST"] == "localhost") {
     define("URL", "/erp_unid");
     define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/erp_unid");
-    define("SERVER", "localhost");
-    define("DB", "erp");
-    define("USER", "root");
-    define("PASSWORD", "");
+    define("SERVER", "servidor1242.il.controladordns.com");
+    define("DB", "sonicbea_erp");
+    define("USER", "sonicbea_tram");
+    define("PASSWORD", "VfQnYRVhg39RCgq");
 } else {
     define("URL", "http://" . $_SERVER["HTTP_HOST"]);
     define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"]);
